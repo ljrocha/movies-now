cbb564ab1d26f431e3020c112c6d8df91e7d9051
ljrocha <ljrocha@users.noreply.github.com>
2020-07-01 11:53:45 -0700

    Add movie description to movie detail view controller.

---
+++ MoviesNow/MNMovieInfoView.swift
@@ -0,0 +1,88 @@
+//
+//  MNMovieInfoView.swift
+//  MoviesNow
+//
+//  Created by Leandro Rocha on 7/1/20.
+//  Copyright © 2020 Leandro Rocha. All rights reserved.
+//
+
+import UIKit
+
+class MNMovieInfoView: UIView {
+    
+    let titleLabel = UILabel(frame: .zero)
+    let dateLabel = UILabel(frame: .zero)
+    let overviewLabel = UILabel(frame: .zero)
+    let descriptionLabel = UILabel(frame: .zero)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        addSubview(titleLabel)
+        addSubview(dateLabel)
+        addSubview(overviewLabel)
+        addSubview(descriptionLabel)
+        
+        backgroundColor = .secondarySystemBackground
+        layer.cornerRadius = 10
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        titleLabel.adjustsFontForContentSizeCategory = true
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        dateLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+        dateLabel.adjustsFontForContentSizeCategory = true
+        dateLabel.numberOfLines = 0
+        dateLabel.textAlignment = .center
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        overviewLabel.text = "Overview"
+        overviewLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        overviewLabel.adjustsFontForContentSizeCategory = true
+        overviewLabel.numberOfLines = 0
+        overviewLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        descriptionLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        descriptionLabel.adjustsFontForContentSizeCategory = true
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let padding: CGFloat = 15
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            
+            dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            dateLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            dateLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            
+            overviewLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 5),
+            overviewLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            overviewLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: overviewLabel.bottomAnchor, constant: 0),
+            descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            descriptionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding)
+        ])
+    }
+    
+    func set(movie: Movie) {
+        titleLabel.text = movie.title
+        dateLabel.text = movie.releaseDate
+        descriptionLabel.text = movie.description
+    }
+    
+}
--- MoviesNow/MovieDetailViewController.swift
+++ MoviesNow/MovieDetailViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class MovieDetailViewController: UIViewController {
 
     let imageView = UIImageView(frame: .zero)
+    let movieInfoView = MNMovieInfoView(frame: .zero)
     
     var movie: Movie!
     
@@ -40,15 +41,23 @@ class MovieDetailViewController: UIViewController {
     
     private func configure() {
         view.addSubview(imageView)
+        view.addSubview(movieInfoView)
         
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleAspectFill
         imageView.translatesAutoresizingMaskIntoConstraints = false
         
+        movieInfoView.set(movie: movie)
+        
+        let padding: CGFloat = 10
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 0.5)
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 0.5),
+            
+            movieInfoView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -3),
+            movieInfoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            movieInfoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
         ])
     }
 
