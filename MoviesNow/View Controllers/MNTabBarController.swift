d0713dc587cb26dffd271239051d3448da3c5674
ljrocha <ljrocha@users.noreply.github.com>
2020-07-14 10:48:09 -0700

    Update variable name for consistency.

--- MoviesNow/View Controllers/MNTabBarController.swift
+++ MoviesNow/View Controllers/MNTabBarController.swift
@@ -17,10 +17,10 @@ class MNTabBarController: UITabBarController {
     }
     
     private func createTopRatedListNavigationController() -> UINavigationController {
-        let topRatedViewController = TopRatedListViewController()
-        topRatedViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 0)
+        let topRatedListViewController = TopRatedListViewController()
+        topRatedListViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 0)
         
-        return UINavigationController(rootViewController: topRatedViewController)
+        return UINavigationController(rootViewController: topRatedListViewController)
     }
     
     private func createMovieSearchListNavigationController() -> UINavigationController {
